c78e7eb2977deecf371471eec201c1ed9bc4ecd6
Aleksey <42502250+fimonlark@users.noreply.github.com>
2019-04-14 22:31:15 +0300

    add scrapper for hrefs and browser opener

--- news_getter.py
+++ news_getter.py
@@ -1,18 +1,16 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
+import webbrowser
 
 class NewsGetter():
-    def __init__(self):
+    def __init__(self, **kwargs):
+        self.page = urllib.request.urlopen('https://www.foxbusiness.com/category/opinion')
         self.news_text = []
 
 
     def get_news(self):
-        page = urllib.request.urlopen('https://www.foxbusiness.com/category/opinion')
-
-        soup = BeautifulSoup(page, 'html.parser')
-
-        name_box = soup.find_all('div', class_='info')
+        soup = BeautifulSoup(self.page, 'html.parser')
+        name_box = soup.find_all('div', class_='info', limit=9)
 
         for i in range(len(name_box)):
             self.news_text.append(name_box[i].get_text())
@@ -20,3 +18,24 @@ class NewsGetter():
         with open('news.txt', 'w') as f:
             f.write(str(self.news_text))
 
+
+    def get_href(self):
+        news_hrefs = []
+        soup = BeautifulSoup(self.page, features="lxml")
+        elems = soup.select("div.info h3 a", limit=9)
+        for i in range(len(elems)):
+            news_hrefs.append(elems[i].attrs["href"])
+
+        with open('hrefs.txt', 'w') as f:
+            f.write(str(news_hrefs))
+
+        print(news_hrefs)
+        return news_hrefs
+
+
+
+    def open_browser(self, href):
+        webbrowser.open_new_tab(href)
+
+
+NewsGetter().get_href()
