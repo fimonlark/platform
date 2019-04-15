95abd2bb23853b3c2afc022b1bc5659644d681d4
Aleksey <42502250+fimonlark@users.noreply.github.com>
2019-04-15 20:39:23 +0300

    Update news_getter.py
    Now get_news() returns only a tag of news

--- news_getter.py
+++ news_getter.py
@@ -9,11 +9,14 @@ class NewsGetter():
 
 
     def get_news(self):
-        soup = BeautifulSoup(self.page, 'html.parser')
-        name_box = soup.find_all('div', class_='info', limit=9)
 
-        for i in range(len(name_box)):
-            self.news_text.append(name_box[i].get_text())
+        soup = BeautifulSoup(self.page, features="lxml")
+        elems = soup.select("div.info h3 a", limit=9)
+        for i in range(len(elems)):
+            self.news_text.append(elems[i].get_text())
+
+        print(self.news_text)
+        #print(news_hrefs)
 
         with open('news.txt', 'w') as f:
             f.write(str(self.news_text))
@@ -21,21 +24,21 @@ class NewsGetter():
 
     def get_href(self):
         news_hrefs = []
+        aTeg = []
         soup = BeautifulSoup(self.page, features="lxml")
         elems = soup.select("div.info h3 a", limit=9)
         for i in range(len(elems)):
+            aTeg.append(elems[i].get_text())
             news_hrefs.append(elems[i].attrs["href"])
 
         with open('hrefs.txt', 'w') as f:
             f.write(str(news_hrefs))
 
-        print(news_hrefs)
+        print(aTeg)
+        #print(news_hrefs)
         return news_hrefs
 
 
 
     def open_browser(self, href):
         webbrowser.open_new_tab(href)
-
-
-NewsGetter().get_href()
